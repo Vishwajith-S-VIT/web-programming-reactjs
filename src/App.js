9ccd6a7a25424e4c9e0b3271aff2aa79ba93e5f8
Vishwajith-S-VIT <vishwajith.s2023@vitstudent.ac.in>
2025-03-16 16:40:52 +0530

    Added Q1 of Ex-1

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const loadComponent = (folder, file) => {
 function DynamicTable() {
   return (
     <div>
-      <h1 align='center'>My React Exercises</h1>
+      <h1 align='center'>My React Exercises </h1>
       <table border="2" cellSpacing={0} cellPadding={5} align='center'>
         <thead>
           <tr>
